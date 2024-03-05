be4843c6004423bca0c9ba80a3e8edd0fe26d7c4
Andrew Ealovega <Andrew@Ealovega.dev>
2024-03-05 13:18:38 -0500

    Add model

--- DriveShareWeb/orm/connect.py
+++ DriveShareWeb/orm/connect.py
@@ -1,7 +1,9 @@
 import datetime
 
 from sqlmodel import create_engine, SQLModel, Session, select
-from ..security.password import password_context
+from ..security.password import hash_password
+# Required to create tables below
+from ..orm.model import *
 
 sqlite_file_name = "db.sqlite"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -13,3 +15,19 @@ def prepare_db():
     # Create tables if not exists
     SQLModel.metadata.create_all(engine)
 
+    with Session(engine) as sess:
+        # Populate DB with demo accounts if not done so far
+        if not sess.get(Account, "avealov@umich.edu"):
+            a1 = Account(email="avealov@umich.edu", password=hash_password("123"), secq1="asd", secq2="asd",
+                         secq3="asd")
+            sess.add(a1)
+
+            l1 = Listing(owner=a1.email, model="Mazda Miata", year=2004, mileage=50000, location="Dearborn, MI",
+                         price=40.5)
+            sess.add(l1)
+            sess.commit()
+
+            d1 = AvailableDateRange(listing_id=l1.id, start_date=datetime.datetime(2024, 5, 22),
+                                    end_date=datetime.datetime(2024, 8, 12))
+            sess.add(d1)
+            sess.commit()
--- DriveShareWeb/orm/model.py
+++ DriveShareWeb/orm/model.py
@@ -1,3 +1,6 @@
+import datetime
+from typing import Optional
+
 from sqlmodel import SQLModel, Field
 from sqlalchemy import PrimaryKeyConstraint
 
@@ -11,3 +14,46 @@ class Account(AccountDTO, table=True):
     secq1: str
     secq2: str
     secq3: str
+
+
+class Listing(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    owner: str = Field(foreign_key="account.email")
+    model: str
+    year: int
+    mileage: int
+    """Miles on car"""
+    location: str
+    """Unformatted location"""
+    price: float
+    """Price per day"""
+
+
+class Reservation(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    owner: str = Field(foreign_key="account.email")
+    """The account reserving"""
+    listing_id: int = Field(foreign_key="listing.id")
+    """ID of listing this reservation is for"""
+    start_date: datetime.datetime
+    end_date: datetime.datetime
+
+
+class Review(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    owner: str = Field(foreign_key="account.email")
+    """Reviewer email"""
+    reservation_id: int = Field(foreign_key="reservation.id")
+    """Reservation this review is for"""
+    text: str
+    """Review text"""
+    rating: int
+    """1-5 star rating"""
+
+
+class AvailableDateRange(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    listing_id: int = Field(foreign_key="listing.id")
+    """Listing this date range is valid for"""
+    start_date: datetime.datetime
+    end_date: datetime.datetime
