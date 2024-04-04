f725babde138b74251694619dec7067acda385f5
Thunderwhere <brandonrshady@gmail.com>
2024-04-04 18:17:30 -0400

    Review Page Added

--- main.py
+++ main.py
@@ -83,6 +83,13 @@ async def home_page(account=Depends(get_current_user)):
 
     return HTMLResponse(content=str.join("", html), status_code=200)
 
+@app.get("/newreview", response_class=HTMLResponse)
+async def home_page(account=Depends(get_current_user)):
+    with open("DriveShareWeb/static/newreview.html") as f:
+        html = f.readlines()
+
+    return HTMLResponse(content=str.join("", html), status_code=200)
+
 
 @app.get("/login", response_class=HTMLResponse)
 async def login_page(not_login=Depends(ensure_user_not_logged_in)):
